e573ba54e8cd2ec3b3e235da470dcad956d025cd
NAGAMANI19 <24P31F00D5@acet.ac.in>
2026-06-09 21:12:13 +0530

    Implement FastAPI application for Iris classification with prediction endpoint

---
+++ app.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+
+# -----------------------------
+# Create FastAPI App
+# -----------------------------
+
+app = FastAPI()
+
+# -----------------------------
+# Load Saved Objects
+# -----------------------------
+
+model = joblib.load("model.pkl")
+scaler = joblib.load("scaler.pkl")
+pca = joblib.load("pca.pkl")
+
+# -----------------------------
+# Request Schema
+# -----------------------------
+
+class IrisInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+# -----------------------------
+# Home Endpoint
+# -----------------------------
+
+@app.get("/")
+def home():
+    return {
+        "message": "Iris Classification API is running successfully!"
+    }
+
+# -----------------------------
+# Prediction Endpoint
+# -----------------------------
+
+@app.post("/predict")
+def predict(data: IrisInput):
+
+    df = pd.DataFrame(
+        [[
+            data.sepal_length,
+            data.sepal_width,
+            data.petal_length,
+            data.petal_width
+        ]],
+        columns=[
+            "sepal length (cm)",
+            "sepal width (cm)",
+            "petal length (cm)",
+            "petal width (cm)"
+        ]
+    )
+
+    scaled = scaler.transform(df)
+
+    transformed = pca.transform(scaled)
+
+    prediction = model.predict(transformed)[0]
+
+    return {
+        "prediction": prediction
+    }
